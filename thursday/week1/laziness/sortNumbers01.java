f7cb7ebe2a12067bef904154d8d86280ac79f675
zhu <71738723+some1mine@users.noreply.github.com>
2022-11-14 15:22:10 +0900

    Update sortNumbers01.java

--- thursday/week1/laziness/sortNumbers01.java
+++ thursday/week1/laziness/sortNumbers01.java
@@ -1,1 +1,22 @@
+import java.io.*;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int len = Integer.parseInt(br.readLine());
+        int[] numbers = new int[len];
+        
+        for(int i = 0 ; i < len ; i++) 
+            numbers[i] = Integer.parseInt(br.readLine());
+        
+        int[] count = new int[2001]; // -1000 ~ 1000
+        
+        for(int i = 0 ; i < numbers.length ; i++) {
+            count[numbers[i] + 1000] += 1;
+        }
+        for (int i = 0 ; i < count.length ; i++) {
+            for (int j = 0 ; j < count[i] ; j++) 
+                System.out.println(i - 1000);
+        }
+    }
+}
