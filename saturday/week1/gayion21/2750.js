c1b084b32398c44dd96b3950b1d49fad31c14ba7
gayion21 <skykoko17@naver.com>
2022-11-14 23:03:49 +0900

    [고구마_Q2750]

--- saturday/week1/gayion21/2750.js
+++ saturday/week1/gayion21/2750.js
@@ -7,7 +7,10 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n'); // �
 input.shift();
 // 전달받은 수의 개수부분 날려버리기~~!
 
-input.sort((a, b) => a - b);  // 화살표 함수로 오름차순 표현
+input.sort((a, b) => a - b);  
+// 첫 번째 요소가 두 번째 요소보다 우선순위가 높으면 음수를 반환, 낮으면 양수를 반환하고 같으면 0을 반환
+// TimeSort 사용 : 시간복잡도 O(n log n), stable, in-place
+// insertion sort와 merge sort를 결합하여 만든 정렬
 
 for (let i = 0; i < input.length; i++) {
   console.log(input[i]);
