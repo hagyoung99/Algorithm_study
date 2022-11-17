130dc7422a2dad85a44eb9b75356220d634af551
zhu <71738723+some1mine@users.noreply.github.com>
2022-11-17 21:45:28 +0900

    Create Q2751.java
    퀵하게 퀵소트

---
+++ thursday/week1/laziness/Q2751.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+		boolean[] arr = new boolean[2000001];	
+        
+		int n = Integer.parseInt(br.readLine());
+        
+		for(int i = 0 ; i < n ; i++)
+			arr[Integer.parseInt(br.readLine()) + 1000000] = true;
+ 
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i]) sb.append((i - 1000000)).append('\n');
+		}
+		System.out.print(sb);
+	}
+}
