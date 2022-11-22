0e240b4dba44611cfdb9beca916c77f27af66cc9
zhu <71738723+some1mine@users.noreply.github.com>
2022-11-22 15:43:11 +0900

    Create Q1427.java
    어떻게 하긴 했는데,,
    partition의 while문 순서 바꾸면 안먹는 게 왜인지 살펴주실분..?

---
+++ thursday/week2/laziness/Q1427.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        sort(arr, 0, arr.length - 1);
+        System.out.println(String.valueOf(arr));
+    }
+    public static void sort(char[] arr, int left, int right) {
+        if(left >= right) return;
+
+        int pivot = partition(arr, left, right);
+        sort(arr, left, pivot - 1);
+        sort(arr, pivot + 1, right);
+    }
+    public static int partition(char[] arr, int left, int right) {
+        int low = left;
+        int high = right;
+        char pivot = arr[left];
+        while(low < high) {
+            while(low < high && arr[high] < pivot) high -= 1;
+            while(low < high && arr[low] >= pivot) low += 1;
+            swap(arr, low, high);
+        }
+        swap(arr, left, low);
+        return low;
+    }
+    public static void swap(char[] arr, int low, int high) {
+        char tmp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = tmp;
+    }
+}
