5d44ebec1e30d63ae4c523cf592fe7b006dd3221
hagyoung <hagyoung99@gmail.com>
2022-11-24 16:17:46 +0900

    Q1181, Q11651

--- thursday/week2/hagyoung/Q11651.java
+++ thursday/week2/hagyoung/Q11651.java
@@ -2,18 +2,17 @@ package thursday.week2.hagyoung;
 
 import java.util.*;
 import java.io.*;
-
-public class Q11650 {
+public class Q11651 {
     public static void main(String[] args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(in.readLine());
         List<Point> answer = new ArrayList<>();
         for(int i=0;i<n;i++){
             String[] xy = in.readLine().split(" ");
-            answer.add(new Point(Integer.parseInt(xy[0]), Integer.parseInt(xy[1])));
+            answer.add(new Point(Integer.parseInt(xy[1]), Integer.parseInt(xy[0])));
         }
         Collections.sort(answer);
-        for(Point x : answer) System.out.println(x.x +" "+x.y);
+        for(Point x : answer) System.out.println(x.y +" "+x.x);
     }
 }
 
@@ -26,12 +25,14 @@ class Point  implements Comparable<Point>{
 
     @Override
     public int compareTo(Point o){
-        if(this.x==o.x){
-            if(this.y==o.y) return 0;
-            else if(this.y<o.y) return -1;
+        if(this.y==o.y) {
+            if(this.x==o.x) return 0;
+            else if(this.x<o.x) return -1;
             else return 1;
         }
-        else if(this.x<o.x) return -1;
+        else if(this.y<o.y) return -1;
         else return 1;
+
+        
     }
 }
