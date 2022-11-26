2ac0843af33f85ab172a081ec63168de554fc6e3
gayion21 <skykoko17@naver.com>
2022-11-26 23:27:40 +0900

    no message

--- saturday/week2/gayion/1427.js
+++ saturday/week2/gayion/1427.js
@@ -1,3 +1,3 @@
 let input = require('fs').readFileSync('dev/stdin').toString().trim().split('').map(n => parseInt(n));
 
-console.log(input.sort((a, b) => b - a).join(''));
+console.log(input.sort((a, b) => b-a).join(''));
