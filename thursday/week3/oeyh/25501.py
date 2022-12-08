ebecdf579b0b0288b6219ed5279dec02a196a375
oeyh98 <skjrjr123@naver.com>
2022-12-08 21:29:12 +0900

    oeyh week4

--- thursday/week3/oeyh/25501.py
+++ thursday/week3/oeyh/25501.py
@@ -7,12 +7,9 @@ def recursion(s, l, r):
     global cnt
     cnt += 1
 
-    if l >= r:
-        return 1
-    elif s[l] != s[r]:
-        return 0
-    else:
-        return recursion(s, l + 1, r - 1)
+    if l >= r : return 1
+    elif s[l] != s[r]: return 0
+    else: return recursion(s, l + 1, r - 1)
 
 def isPalindrome(s):
     return recursion(s, 0, len(s) - 1)
