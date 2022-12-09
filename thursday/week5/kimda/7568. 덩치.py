31458ab1a3a38decf881c88a37c4f17a9766faa3
helen121027 <helen121027@gmail.com>
2022-12-09 20:35:39 +0900

    Update Q7568

---
+++ thursday/week5/kimda/7568. 덩치.py
@@ -0,0 +1,13 @@
+N = int(input())
+people = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    people.append((x, y))
+
+for i in people:
+    rank = 1
+    for j in people:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=" ")
