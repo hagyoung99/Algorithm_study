1d51ea64b9bf66a6b31633e069cba9872c542923
이동원 <104751794+movingone@users.noreply.github.com>
2022-12-10 21:34:51 +0900

    덩치

---
+++ saturday/week4/dongwon/BaekJoon7568.java
@@ -0,0 +1,37 @@
+package Brutforth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BaekJoon7568 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int arr[][] = new int[n][2];
+
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            int rank = 1;
+            for (int j = 0; j < n; j++) {
+
+                if (i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            System.out.print(rank + " ");
+        }
+    }
+}
