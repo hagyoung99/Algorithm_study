26624a4a9d0315443814d81fff5c4b29a0301014
김하경 <hagyoung99@gmail.com>
2022-12-30 23:15:16 +0900

    Create Q10773.java

---
+++ thursday/week8/hagyoung99/Q10773.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(in.readLine());
+        int[] arr = new int[k];
+        for(int i=0;i<k;i++) arr[i] = Integer.parseInt(in.readLine());
+
+        int answer = 0;
+        Stack<Integer> st = new Stack<>();
+        for(int x:arr){
+            if(x!=0) st.push(x);
+            else st.pop();
+        }
+        for(int i=0;i<st.size();i++) answer += st.get(i);
+        System.out.println(answer);
+    }
+}
