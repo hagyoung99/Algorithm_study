a4a734525c4ed0dcfb80240f3f45df817670cbb0
gayion21 <skykoko17@naver.com>
2023-01-07 23:07:45 +0900

    no message

--- saturday/week7/gayion/Q1874.js
+++ saturday/week7/gayion/Q1874.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-
+/* 
+  8
+  4 3 6 8 7 5 2 1
+*/
 function getResult() {
   let m = 1;
   let stack = [];
@@ -8,8 +11,7 @@ function getResult() {
 
   for (let i = 1; i <= parseInt(input[0]); i++) {
     if (
-      parseInt(input[i]) >
-      (stack[stack.length - 1] === undefined ? 0 : stack[stack.length - 1])
+      parseInt(input[i]) > (stack[stack.length - 1] === undefined ? 0 : stack[stack.length - 1])
       // stack에 마지막 배열에 숫자가 없거나, 있어도 input[i]가 더 크면
     ) {
       for (let j = m; j <= parseInt(input[i]); j++) {
@@ -20,6 +22,7 @@ function getResult() {
     } else if (parseInt(input[i]) < stack[stack.length - 1]) {
       return "NO";
     }
+
     if (parseInt(input[i]) === stack[stack.length - 1]) {
       // input[i]와 stack의 마지막 수가 같아지면 pop
       result.push("-");
