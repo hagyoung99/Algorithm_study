bf51d930190f3d1e45ca756629680a816c96a4d8
oeyh98 <skjrjr123@naver.com>
2023-01-14 18:00:57 +0900

    oeyh

---
+++ thursday/week10/1920수찾기.py
@@ -0,0 +1,25 @@
+import sys
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return 0
+
+    mid = (start + end) //2
+    if array[mid] == target:
+        return 1
+    elif array[mid] < target:
+        return binary_search(array, target, mid +1, end)
+    else:
+        return binary_search(array, target, start, mid-1)
+
+
+N = int(sys.stdin.readline())
+array = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+target_array = list(map(int, sys.stdin.readline().split()))
+
+array.sort()
+
+for target in target_array:
+    print(binary_search(array, target, 0, N-1))
+
