60139a45137ae3a7539ca2ecf1dc607a3439ff22
hagyoung <hagyoung99@gmail.com>
2023-01-25 23:49:14 +0900

    Q1927, Q11279

---
+++ thursday/week11/hagyoung/Q11279.java
@@ -0,0 +1,23 @@
+package thursday.week11.hagyoung;
+
+import java.io.*;
+import java.util.*;
+public class Q11279 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        while(N-->0){
+            int x = Integer.parseInt(br.readLine());
+            if(x==0){
+                if(pq.isEmpty()) sb.append("0\n");
+                else sb.append(pq.poll()).append("\n");
+            } else pq.add(x);
+        }
+
+        System.out.println(sb);
+    }
+}
