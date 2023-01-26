5cfce1ef3eae247769a20b4a40de8823b9d58bed
helen121027 <helen121027@gmail.com>
2023-01-26 21:48:53 +0900

    Update Q1927 주석 추가

--- thursday/week11/kimda/1927. 최소 힙.py
+++ thursday/week11/kimda/1927. 최소 힙.py
@@ -1,5 +1,9 @@
+# 파이썬 heapq 내장 모듈은 이진 트리 기반의 최소 힙 자료구조를 제공한다.
 import heapq
+import sys
+input = sys.stdin.readline
 n = int(input())
+# 최소 힙 생성
 heap = []
 result = []
 
@@ -7,10 +11,8 @@ for _ in range(n):
     num = int(input())
     if num == 0:
         if heap:
-            result.append(heapq.heappop(heap))
+            print(heapq.heappop(heap))
         else:
-            result.append(0)
+            print(0)
     else:
         heapq.heappush(heap, num)
-for num in result:
-    print(num)
