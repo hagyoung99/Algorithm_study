47330133af07d682f5b8c00006f8c5aaa5daca64
oeyh98 <skjrjr123@naver.com>
2023-02-04 03:02:47 +0900

    oeyh week 13 - 2

--- thursday/week13/oeyh/24479_깊이우선탐색1.py
+++ thursday/week13/oeyh/24479_깊이우선탐색1.py
@@ -1,23 +1,31 @@
 import sys
-n, m ,r = map(int, sys.stdin.readline().split()) #정점의 수, 간선의 수, 시작 정점
-u, v = list(), list() # 정점u, 정점v 가중치 1, 양방향 간선
-#정점 번호를 오름차순으로!! 방문
-#내림차순 정렬 -> 내림차순 후, 후입선출 전략을 사용하면 오름차순과 같게됨 = 성능 증가
+# 정점u, 정점v 가중치 1, 양방향 간선
 #시작 정점에서 방문할 수 없는 노드는 0!!!
-
+#정점의 수, 간선의 수, 시작 정점
+sys.setrecursionlimit(10**9)
+n, m, r = map(int, sys.stdin.readline().split())
 graph =[[] for _ in range(n+1)]
 visited = [0] * (n+1)
+count = 1
 
-for _ in range(m):#2차원배열형태 저장
+for _ in range(m):  # 2차원배열형태 저장
     u, v = map(int, sys.stdin.readline().split())
     graph[u].append(v)
     graph[v].append(u)
 
-def dfs(graph, start_node, visited):
-    need_visit= list()
-    graph[start_node].sort()
+#정점 번호를 오름차순으로!! 방문
+#내림차순 정렬 -> 내림차순 후, 후입선출 전략을 사용하면 오름차순과 같게됨 = 성능 증가
+def dfs(v):
+    global count
+    visited[v] = count
+    graph[v].sort(reverse=True)
 
-result = dfs(graph, r, visited)
-for i in result:
-    print(i)
+    for g in graph[v]:
+        if visited[g] == 0:
+            count += 1
+            dfs(g)
+
+dfs(r)
+
+print(*visited[1:], sep="\n")
 
--- thursday/week13/oeyh/24480_깊이우선탐색2.py
+++ thursday/week13/oeyh/24480_깊이우선탐색2.py
@@ -1,53 +1,25 @@
 import sys
-
-input = sys.stdin.readline
-
-N, M, R = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-# 내림차순인 인접 그래프를 스택에 하나씩 넣어주면, 나중에 스택에서
-# 꺼낼때는 이들을 오름차순으로 꺼내게 됨
-for i in range(1, len(graph)):
-
-    graph[i].sort(reverse=True)
+sys.setrecursionlimit(10**9)
+n, m, r = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+count = 1
+for _ in range(m):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
 
-def DFS(start):
-    stack = [start]
-    visited = [-1] * (N + 1)
-    result = [0] * (N + 1)  # result[idx]는 idx노드를 방문한 순서 값을 의미함
-    cnt = 1  # 방문 순서 값
+def dfs(r):
+    global count
+    visited[r] = count
+    graph[r].sort(reverse=True)
 
-    while stack:
-        cnt_node = stack.pop()
-        # 사이클이 있는 그래프 같은 경우에,
-        # 어떤 노드에 대해, 그 노드의 인접 노드 K를 스택에 넣어
-        # 놓은 뒤에, DFS를 수행하다가 더 깊은 깊이의 어느 노드에서
-        # 인접 노드로 K를 또 스택에 넣는 경우가 생김.
-        # 이 때의 K가 먼저 스택에서 꺼내져 처리되므로,
-        # 맨 첨 넣어줬던 K는 스택에서 꺼낸 뒤 따로 코드를
-        # 수행해주지 않고 넘어감
-        if visited[cnt_node] == 1:
-            continue
+    for g in graph[r]:
+        if visited[g] == 0:
+            count += 1
+            dfs(g)
 
-        visited[cnt_node] = 1
+dfs(r)
 
-        # 방문 순서 값 저장
-        result[cnt_node] = cnt
-        cnt += 1
-
-        # 아직 방문 이력 없으면(=스택에서 뽑은 적 없으면)
-        # 스택에 싹 다 집어 넣기
-        for adj_node in graph[cnt_node]:
-            if visited[adj_node] == -1:
-                stack.append(adj_node)
-
-    return result
-
-
-result = DFS(R)
-
-print(*result[1:], sep="\n")
+print(*visited[1:], sep="\n")
