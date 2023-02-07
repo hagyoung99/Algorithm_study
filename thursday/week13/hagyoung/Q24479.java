32a21cc3fd38e153a0477baa167ce0a925a3460b
hagyoung <hagyoung99@gmail.com>
2023-02-07 22:23:42 +0900

    Q24479

---
+++ thursday/week13/hagyoung/Q24479.java
@@ -0,0 +1,53 @@
+package thursday.week13.hagyoung;
+
+import java.io.*;
+import java.util.*;
+class Q24479{
+    static int[] visit;
+    static int count;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   //정점의 수
+        int M = Integer.parseInt(st.nextToken());   //간선의 수
+        int R = Integer.parseInt(st.nextToken());   //시작 정점
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        visit = new int[N+1];
+
+        for(int i = 0;i<=M;i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<M;i++){   //간선별 노드 저장
+            st = new StringTokenizer(br.readLine());
+            int to = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            list.get(to).add(from);
+            list.get(from).add(to);
+        }
+
+        for(int i=1;i<=N;i++){  //정렬
+            Collections.sort(list.get(i));
+        }
+
+        count = 1;
+        DFS(R, list);
+
+        for(int i=1;i<=N;i++){
+            System.out.println(visit[i]);
+        }
+    }
+
+    static void DFS(int R, ArrayList<ArrayList<Integer>> list){
+        visit[R] = count;
+        for(int i = 0;i<list.get(R).size();i++){
+            int newNode = list.get(R).get(i);
+            if(visit[newNode]==0){
+                count++;
+                DFS(newNode, list);
+            }
+        }
+    }
+
+}
