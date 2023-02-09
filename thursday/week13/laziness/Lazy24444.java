789a6ee483e1713d90e79a23f71ff090001dc1aa
zhu <parkjw573@naver.com>
2023-02-09 22:27:42 +0900

    message

--- thursday/week13/laziness/Lazy24444.java
+++ thursday/week13/laziness/Lazy24444.java
@@ -2,7 +2,6 @@ package thursday.week13.laziness;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
 
 public class Lazy24444 {
     public static boolean[] visited;
@@ -17,9 +16,9 @@ public class Lazy24444 {
         ord = new int[n];
         int m = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
-        int[] nodes = IntStream.range(1, n + 1).toArray();
         edges = new ArrayList[n];
         for(int i = 0 ; i < n ; i++) edges[i] = new ArrayList<>();
+
         for(int i = 0 ; i < m ; i++) {
             st = new StringTokenizer(reader.readLine());
             int u = Integer.parseInt(st.nextToken());
@@ -27,12 +26,14 @@ public class Lazy24444 {
             edges[u - 1].add(v - 1);
             edges[v - 1].add(u - 1);
         }
+
         for(int i = 0 ; i < n ; i++) Collections.sort(edges[i]);
-        bfs(nodes, r);
+
+        bfs(r);
         for(int i : ord) System.out.println(i);
         reader.close();
     }
-    private static void bfs(int[] nodes, int r) {
+    private static void bfs(int r) {
         visited[r - 1] = true;
         ord[r - 1] = idx++;
         Queue<Integer> queue = new LinkedList<>();
