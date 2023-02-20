c968bcddb04b60128b9cd98fda48b5241d6f8137
NutellaCode <unitedsslee@gmail.com>
2023-02-20 18:12:48 +0900

    issed Q20922 upload

--- saturday/week13/XiLee/XiLee/src/BOJ_20922.java
+++ saturday/week13/XiLee/XiLee/src/BOJ_20922.java
@@ -1,5 +1,32 @@
-public class BOJ_20922 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class BOJ_20922 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int[] cnt = new int[100001];
+
+        int answer = 0, start = 0, end = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        while (end != n) {
+            while (end < n && cnt[arr[end]] + 1 <= k) {
+                cnt[arr[end++]]++;
+            }
+            if (end - start > answer) answer = end - start;
+            cnt[arr[start++]]--;
+        }
+        System.out.println(answer);
     }
 }
--- saturday/week13/XiLee/XiLee/src/BOJ_6159.java
+++ saturday/week13/XiLee/XiLee/src/BOJ_6159.java
@@ -1,9 +1,11 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_6159 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
