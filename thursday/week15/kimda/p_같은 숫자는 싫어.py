b8ac53d232ef931d476489e44858f1b8db5eaa7f
helen121027 <helen121027@gmail.com>
2023-02-22 01:57:21 +0900

    UPDATE p_같은 숫자는 싫어

---
+++ thursday/week15/kimda/p_같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    for i in range(1, len(arr)):
+        if arr[i] == arr[i - 1]:
+            continue
+        else:
+            answer.append(arr[i])
+    return answer
