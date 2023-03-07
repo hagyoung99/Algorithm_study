86040cfdb0b8e78a5a6ecc7b556808d5e20674bf
해철 <102534186+lamodadite@users.noreply.github.com>
2023-03-07 23:34:05 +0900

    Create 9655번 돌게임.java

---
+++ thursday/week17/haechul/9655번 돌게임.java
@@ -0,0 +1,10 @@
+package BOJ.boj9655;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() % 2 == 0 ? "CY" : "SK");
+    }
+}
