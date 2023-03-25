b52bd7ddeb661fb86c27e21b899d24636fec5d29
NutellaCode <unitedsslee@gmail.com>
2023-03-25 20:28:44 +0900

    1st add

--- saturday/week_16/XiLee/src/BOJ_20444.java
+++ saturday/week_16/XiLee/src/BOJ_20444.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class BOJ_20444 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+
+        
+    }
 }
--- saturday/week_16/XiLee/src/BOJ_7453.java
+++ saturday/week_16/XiLee/src/BOJ_7453.java
@@ -1,2 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_7453 {
+    static StringTokenizer st;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        for(int i = 0 ; i < n ; i++){
+
+        }
+
+
+    }
+
 }
+
