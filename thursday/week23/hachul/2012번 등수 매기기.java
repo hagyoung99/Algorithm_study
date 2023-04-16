9e549a9d33fd361259d73e7596d61cad5f7a9fdc
해철 <102534186+lamodadite@users.noreply.github.com>
2023-04-17 02:36:55 +0900

    Create 2012번 등수 매기기.java

---
+++ thursday/week23/hachul/2012번 등수 매기기.java
@@ -0,0 +1,27 @@
+package BOJ.boj2012;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] expected = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            expected[i] = sc.nextInt();
+        }
+        Arrays.sort(expected);
+
+        System.out.println(IntStream.range(1, n + 1).mapToLong(i -> Math.abs(i - expected[i - 1])).sum());
+
+
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += Math.abs(i - expected[i - 1]);
+        }
+        System.out.println(sum);
+    }
+}
