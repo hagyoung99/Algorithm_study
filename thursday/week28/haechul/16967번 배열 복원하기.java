ca4aa8dbc7594eb79f5c859a582929014838b026
해철 <102534186+lamodadite@users.noreply.github.com>
2023-05-23 00:40:58 +0900

    Create 16967번 배열 복원하기.java

---
+++ thursday/week28/haechul/16967번 배열 복원하기.java
@@ -0,0 +1,37 @@
+package BOJ.boj16967;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        int[][] arr = new int[H + X][W + Y];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = X; i < H; i++) {
+            for (int j = Y; j < W; j++) {
+                arr[i][j] -= arr[i - X][j - Y];
+            }
+        }
+
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
